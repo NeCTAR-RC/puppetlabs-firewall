1f8e49427a9251baa945fc987caa65c8b7d6028e
Sam Morrison <sorrison@gmail.com>
2024-08-16 11:41:21 +1000

    Allow lowercase accept/reject/drop in jump too

--- lib/puppet/provider/firewall/iptables.rb
+++ lib/puppet/provider/firewall/iptables.rb
@@ -792,7 +792,7 @@ Puppet::Type.type(:firewall).provide :iptables, parent: Puppet::Provider::Firewa
 
     # If the jump parameter is set to one of: ACCEPT, REJECT or DROP then
     # we should set the action parameter instead.
-    if ['ACCEPT', 'REJECT', 'DROP'].include?(hash[:jump])
+    if ['ACCEPT', 'REJECT', 'DROP', 'accept', 'reject', 'drop'].include?(hash[:jump])
       hash[:action] = hash[:jump].downcase
       hash.delete(:jump)
     end
